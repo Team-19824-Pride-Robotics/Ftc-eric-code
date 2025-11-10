90e0e72f75a08c1d8e59a76efed4b0c58f3d73ff
Eazy667 <eric_jones.stu@willowschoolnola.org>
2025-11-10 15:51:42 -0600

    Create OnePlayer_TeleOp.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OnePlayer_TeleOp.java
@@ -0,0 +1,247 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+@TeleOp(name="OnePlayer_TeleOp")
+@Configurable
+
+
+
+public class OnePlayer_TeleOp extends LinearOpMode {
+
+    // Declare OpMode members.
+    int test = 0;
+    int intakeBack;
+
+   private Servo LegServo;
+    private DcMotor FL;
+    private DcMotor FR;
+    private DcMotor BL;
+    private DcMotor BR;
+    private DcMotor transfer;
+    private DcMotor intake;
+    private DcMotorEx fly1;
+    private DcMotorEx fly2;
+    private Limelight3A limelight;
+//    private IMU imu;
+
+
+    //Declare variables
+
+    public static double backOffSpeed = -600;
+    public static double transferback = -1;
+    public static double long_launch_speed = 2110;
+    public static double close_launch_speed = 1650;
+    public static double intakeOn = 1;
+    public static double transferOn = 1;
+    double fly1Speed = 0;
+    double fly2Speed = 0;
+    int intakePosition = 0;
+    double transferSpeed = 0;
+    double transferRuntime = 3;
+    double Balldown = 0.6;
+    int transferPosition;
+
+    int indexPosition;
+    final double TURN_GAIN = 0.01;
+    final double MAX_AUTO_TURN = 0.3;
+
+
+    @Override
+    public void runOpMode() {
+
+        // Initialize the hardware variables.
+
+        BL = hardwareMap.get(DcMotor.class, "BL");
+        BR = hardwareMap.get(DcMotor.class, "BR");
+        FL = hardwareMap.get(DcMotor.class, "FL");
+        FR = hardwareMap.get(DcMotor.class, "FR");
+
+        BL.setDirection(DcMotor.Direction.REVERSE);
+
+        
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        transfer = hardwareMap.get(DcMotor.class, "transfer");
+        LegServo = hardwareMap.get(Servo.class, "LegServo");
+        fly1 = hardwareMap.get(DcMotorEx.class, "fly1");
+        fly2 = hardwareMap.get(DcMotorEx.class, "fly2");
+        fly1.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        fly1.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        fly2.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        fly2.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        intake.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        intake.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+
+//        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+//        limelight.pipelineSwitch(8); //this is the april tag
+//        imu = hardwareMap.get(IMU.class, "imu");
+//        RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(
+//                RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+//                RevHubOrientationOnRobot.UsbFacingDirection.DOWN);
+//        imu.initialize((new IMU.Parameters(revHubOrientationOnRobot)));
+
+       // Wait for the game to start (driver presses START)
+        waitForStart();
+
+       //limelight.start();
+
+
+ //////////////This is the start of the loop///////////////////////////
+
+        while (opModeIsActive()) {
+
+
+///////////////////DRIVE CONTROLS///////////////////////////////////
+
+
+            double y = -gamepad2.left_stick_y; // Remember, Y stick is reversed!
+            double x = gamepad2.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad2.right_stick_x;
+
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double leftFrontPower = (y + x + rx) / denominator;
+            double leftBackPower = (y - x + rx) / denominator;
+            double rightFrontPower = (y - x - rx) / denominator;
+            double rightBackPower = (y + x - rx) / denominator;
+
+            FL.setPower(leftFrontPower);
+            BL.setPower(leftBackPower);
+            FR.setPower(rightFrontPower);
+            BR.setPower(rightBackPower);
+
+
+///////////////////FLYWHEEL CONTROLS///////////////////////////////////
+
+            if (gamepad2.right_trigger > .1) {
+                fly1Speed = close_launch_speed;
+                fly2Speed = close_launch_speed;
+                LegServo.setPosition(0);
+            }
+            else if (gamepad2.left_trigger > .1) {
+                LegServo.setPosition(0);
+                fly1Speed = long_launch_speed;
+                fly2Speed = long_launch_speed;
+
+            }else if (gamepad2.x){
+                fly1Speed = backOffSpeed;
+                fly2Speed = backOffSpeed;
+                LegServo.setPosition(0);
+            } else {
+                LegServo.setPosition(.3);
+                fly1Speed = 0;
+                fly2Speed = 0;
+            }
+
+
+///////////////////TRANSFER CONTROLS///////////////////////////////////
+            if(gamepad2.left_bumper) {
+//                    transferPosition = transfer.getCurrentPosition() + 30;
+//
+//                    transfer.setPower(transferOn);
+                transfer.setPower(1);
+
+            }
+
+            else if(gamepad2.right_bumper) {
+//                transferPosition = transfer.getCurrentPosition() - 30;
+//                transfer.setPower(transferback);
+                transfer.setPower(-1);
+            }
+
+            else {
+                transfer.setPower(0);
+            }
+
+
+
+
+
+
+            ///////////////////INTAKE CONTROLS///////////////////////////////////
+
+            if (gamepad2.a || gamepad1.a) {
+
+//                int intakeForward = intake.getCurrentPosition() + 200;
+//                intakePosition = intakeForward;
+                //WE use a encoder so the motor can go to a specific position. Since the Encoder can tell you the extact position of the motor. So tell it to go to that data and BOOM [this is for eric]
+                intake.setPower(intakeOn);
+
+            }
+            else if(gamepad2.b || gamepad1.b) {
+
+//                intakeBack = intake.getCurrentPosition() - 200;
+//                intakePosition = intakeBack;
+//                test = test +1;
+                intake.setPower(-.4);
+
+
+            }
+
+            else {
+                intake.setPower(0);
+            }
+//            intake.setTargetPosition(intakePosition);
+//            intake.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            if (gamepad2.dpad_left || gamepad1.dpad_left) {
+
+                FL.setPower(-.2);
+                FR.setPower(.2);
+                BL.setPower(-.2);
+                BR.setPower(.2);
+            }
+            if (gamepad2.dpad_right || gamepad1.dpad_right) {
+
+                FL.setPower(.2);
+                FR.setPower(-.2);
+                BL.setPower(.2);
+                BR.setPower(-.2);
+            }
+            if (gamepad2.dpad_up || gamepad1.dpad_up) {
+
+                FL.setPower(.2);
+                FR.setPower(.2);
+                BL.setPower(.2);
+                BR.setPower(.2);
+            }
+            if (gamepad2.dpad_down || gamepad1.dpad_down) {
+
+                FL.setPower(-.2);
+                FR.setPower(-.2);
+                BL.setPower(-.2);
+                BR.setPower(-.2);
+            }
+
+
+
+///////////////////MOTOR CONTROLS///////////////////////////////////
+
+            fly1.setVelocity(fly1Speed);
+            fly2.setVelocity(fly2Speed);
+
+            telemetry.addData("Transfer Position", transferPosition);
+            telemetry.addData("Short Target Velocity", close_launch_speed);
+            telemetry.addData("Long Target Velocity", long_launch_speed);
+            telemetry.addData("Current Velocity", fly1.getVelocity());
+            telemetry.addData("Current Velocity", fly2.getVelocity());
+            telemetry.addData("flywheel1 power", fly1.getPower());
+            telemetry.addData("flywheel2 power", fly2.getPower());
+            telemetry.addData("test", test);
+            telemetry.addData("intake back", intakeBack);
+            telemetry.addData("intake position", intakePosition);
+            telemetry.addData("actual intake pos", intake.getCurrentPosition());
+            telemetry.addData("actual target pos", intake.getTargetPosition());
+            telemetry.addData("intake power", intake.getPower());
+            telemetry.update();
+            
+        }
+    }  
+}
