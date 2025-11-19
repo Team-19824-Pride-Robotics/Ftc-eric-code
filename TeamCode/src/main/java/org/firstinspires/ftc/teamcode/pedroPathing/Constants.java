f6ef5cb85c15e6ad924ad370cdd357b2a2535ffd
Eazy667 <eric_jones.stu@willowschoolnola.org>
2025-11-19 16:30:32 -0600

    Update Constants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -37,7 +37,8 @@ public class Constants {
                 0,
                 0,
                 0.0))
-        .centripetalScaling(0.005);
+
+        .centripetalScaling(0);
 
 
 
@@ -54,7 +55,11 @@ public class Constants {
             .xVelocity(78.7)
             .yVelocity(60.5);
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+    public static PathConstraints pathConstraints = new PathConstraints(
+            0.99,
+            100,
+            0.1,
+            1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
