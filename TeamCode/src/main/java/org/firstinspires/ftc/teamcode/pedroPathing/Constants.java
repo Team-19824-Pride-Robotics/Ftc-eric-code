fb9e0200207720752e2e7f9995371525cc327001
Eazy667 <eric_jones.stu@willowschoolnola.org>
2025-12-04 15:40:22 -0600

    Update Constants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -58,7 +58,7 @@ public class Constants {
     public static PathConstraints pathConstraints = new PathConstraints(
             0.99,
             100,
-            0.1,
+            1.4,
             1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
