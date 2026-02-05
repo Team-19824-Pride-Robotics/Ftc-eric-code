63304b3eb4ba194e634048164b2276c48972cef5
Matt Owen <matthew_owen@willowschoolnola.org>
2026-02-05 15:08:23 -0600

    Update launchAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/launchAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/launchAuto.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-//testing
+
 @Disabled
 @Autonomous
 @Configurable
